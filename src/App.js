7356f9c3a4ac9616f26d644f6569ab036af12b8c
Faramarz Javdan <javdanfar@outlook.com>
2025-10-01 20:51:50 +0330

    Game Updated

--- src/App.js
+++ src/App.js
@@ -15,19 +15,40 @@ export default function Board() {
 
   function handleCLick(i) {
     const nextSquare = squares.slice();
-    if (xIsNext) {
-      nextSquare[i] = "x";
-    } else {
-      nextSquare[i] = "o";
+
+    if (squares[i] || calculateWinner(squares)) {
+      return;
     }
+    nextSquare[i] = xIsNext ? "X" : "O";
+
     setSquares(nextSquare);
     setXIsNext(!xIsNext);
-    console.log(nextSquare);
+  }
+
+function isEmpty(squares) {
+  return squares.some((square, index) => {
+    if (square === null) {
+      console.log(`${square} at index ${index} is null`);
+      return true;
+    }
+    return false;
+  });
+}
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner !== null) {
+    status = `Winner is: ${winner}`;
+  } else if (isEmpty(squares)) {
+    status = "Next player is: " + (xIsNext ? "X" : "O");
+  } else {
+    status = "Nobody Win!!!";
   }
 
   return (
     <>
       <div className='container'>
+        <div>{status}</div>
         <div className='board-row'>
           <Square value={squares[0]} onSquareClick={() => handleCLick(0)} />
           <Square value={squares[1]} onSquareClick={() => handleCLick(1)} />
@@ -46,4 +67,25 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function calculateWinner(squares) {
+  const winLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < winLines.length; i++) {
+    const [a, b, c] = winLines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
